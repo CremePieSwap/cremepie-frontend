09b1b7d49b58964f8318a9063a4d0783ac3e2e05
Chiv <vuducchinh29@gmail.com>
2021-08-24 16:19:41 +0700

    update total burned

--- src/views/Home/components/CakeStats.tsx
+++ src/views/Home/components/CakeStats.tsx
@@ -54,7 +54,7 @@ const CakeStats = () => {
       </Subtitle5>
       <Subtitle5>
         <div>Total CPIE Burned</div>
-        <div>157,984,612</div>
+        <div>125,964,717</div>
       </Subtitle5>
       <Subtitle5>
         <div>New CPIE per block</div>
